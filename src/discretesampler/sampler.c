8b4435a8e3fbf92bbda408a7d86f155e4e2af4d8
David R. MacIver <david@drmaciver.com>
2017-01-01 08:23:18 +0000

    Move to murmurhash as our hash function of choice

--- src/discretesampler/sampler.c
+++ src/discretesampler/sampler.c
@@ -158,7 +158,7 @@ size_t random_sampler_sample(random_sampler *sampler, struct mt *mt){
 
 typedef struct {
     random_sampler *sampler;
-    uint64_t hash;
+    uint32_t hash;
     size_t access_date;
 } sampler_entry;
 
@@ -192,14 +192,13 @@ uint64_t hash_double(uint64_t seed, double x){
     return hash64(seed ^ key);
 }
 
-static uint64_t memhash(size_t n, char *bytes){
-    uint64_t lenhash = hash64((uint64_t)n);
-    uint64_t accumulator = 0;
-    for(size_t i = 0; i < n; i += 4){
-        accumulator *= 31;
-        accumulator += ((uint64_t)bytes[i] & 0xff);
-    }
-    return hash64(lenhash ^ accumulator);
+void MurmurHash3_x86_32 ( const void * key, int len,
+                          uint32_t seed, void * out );
+
+static uint32_t memhash(size_t n, char *bytes){
+    uint32_t result;
+    MurmurHash3_x86_32(bytes, n, 0, &result);
+    return result;
 }
 
 static uint64_t hash_doubles(size_t n_items, double* weights){
@@ -210,6 +209,7 @@ static void push_index(sampler_family *family, size_t index){
     assert(family->last_index < RECENCY);
     family->last_index = (family->last_index + 1) & (RECENCY - 1);
     family->recent[family->last_index] = index; 
+    //printf("Writing %zu to %zu\n", index, family->last_index);
 }
 
 #define PROBE_MAX 8
@@ -436,3 +436,82 @@ static uint64_t genrand64_int64(struct mt *r)
 
     return x;
 }
+
+// Following code is from MurmurHash3, which has been placed into the public
+// domain.
+
+uint32_t fmix32 ( uint32_t h )
+{
+  h ^= h >> 16;
+  h *= 0x85ebca6b;
+  h ^= h >> 13;
+  h *= 0xc2b2ae35;
+  h ^= h >> 16;
+
+  return h;
+}
+
+inline uint32_t rotl32 ( uint32_t x, int8_t r )
+{
+  return (x << r) | (x >> (32 - r));
+}
+
+inline uint32_t getblock32 ( const uint32_t * p, int i )
+{
+  return p[i];
+}
+
+
+void MurmurHash3_x86_32 ( const void * key, int len,
+                          uint32_t seed, void * out )
+{
+  const uint8_t * data = (const uint8_t*)key;
+  const int nblocks = len / 4;
+
+  uint32_t h1 = seed;
+
+  const uint32_t c1 = 0xcc9e2d51;
+  const uint32_t c2 = 0x1b873593;
+
+  //----------
+  // body
+
+  const uint32_t * blocks = (const uint32_t *)(data + nblocks*4);
+
+  for(int i = -nblocks; i; i++)
+  {
+    uint32_t k1 = getblock32(blocks,i);
+
+    k1 *= c1;
+    k1 = rotl32(k1,15);
+    k1 *= c2;
+    
+    h1 ^= k1;
+    h1 = rotl32(h1,13); 
+    h1 = h1*5+0xe6546b64;
+  }
+
+  //----------
+  // tail
+
+  const uint8_t * tail = (const uint8_t*)(data + nblocks*4);
+
+  uint32_t k1 = 0;
+
+  switch(len & 3)
+  {
+  case 3: k1 ^= tail[2] << 16;
+  case 2: k1 ^= tail[1] << 8;
+  case 1: k1 ^= tail[0];
+          k1 *= c1; k1 = rotl32(k1,15); k1 *= c2; h1 ^= k1;
+  };
+
+  //----------
+  // finalization
+
+  h1 ^= len;
+
+  h1 = fmix32(h1);
+
+  *(uint32_t*)out = h1;
+} 
